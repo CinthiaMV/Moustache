804e15bb219d79bd8bc9b882faae9e49f1ceff35
VictorSamuel <victor_samuel_@hotmail.com>
2016-06-02 22:20:00 -0300

    Modelo pronto

---
+++ mScript.js
@@ -0,0 +1,20 @@
+function gerarBotoes(){
+	document.getElementById("home").src = "home.png";
+	document.getElementById("home").className = "botao"
+	document.getElementById("h").href="moustache.html";
+	
+	document.getElementById("servico").src = "servicos.png";
+	document.getElementById("servico").className = "botao"
+	document.getElementById("s").href="servico.html";
+	
+	document.getElementById("fotos").src = "fotos.png";
+	document.getElementById("fotos").className = "botao"
+	document.getElementById("f").href="fotos.html";
+	
+	document.getElementById("contato").src = "contato.png";
+	document.getElementById("contato").className = "botao"
+	document.getElementById("c").href="contato.html";
+	
+	document.getElementById("endereco").innerHTML =  "<p>R. José Taváres de Miranda, 130 - Vila Pompeia, São Paulo - SP, 05026-060 </p>";	
+}
+
